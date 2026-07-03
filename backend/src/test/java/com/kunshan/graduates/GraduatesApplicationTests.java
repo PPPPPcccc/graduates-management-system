6be5e78aeb907f405616ea7eee6745e93726603f
pc <pc@pcdeMacBook-Air.local>
2026-07-03 10:19:36 +0800

    Initial commit: 学生就业信息管理系统

---
+++ backend/src/test/java/com/kunshan/graduates/GraduatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunshan.graduates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
